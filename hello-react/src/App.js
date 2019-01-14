297277f9a03bda21cb5de093e956f9b1b9d6b0c7
unknown <ejisvip@gmail.com>
2019-01-14 09:11:50 +0900

    Finish to 9.4 Styled-components

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,25 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import LifeCycleSample from './LifeCycleSample';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
+// function getRandomColor() {
+//   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// }
 
 class App extends Component {
-  state = {
-    color: '#000000'
-  }
+  // state = {
+  //   color: '#000000'
+  // }
 
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  }
+  // handleClick = () => {
+  //   this.setState({
+  //     color: getRandomColor()
+  //   });
+  // }
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color} />
+        <StyledButton big>버튼</StyledButton>
       </div>
     );
   }
