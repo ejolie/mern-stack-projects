1efa0acdfd5b3406505fd314686901b2c845f135
Eunjeong Park <ejisvip@gmail.com>
2019-04-29 02:23:45 +0900

    Create movieql-client project

---
+++ movieql-client/README.md
@@ -0,0 +1,5 @@
+# MovieQL Client
+
+Make a movie app with GraphQL, Apollo
+
+from [노마드코더] GraphQL, Apollo로 웹앱 만들기 course
---
+++ movieql-client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import client from './apolloClient';
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
