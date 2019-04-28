d2f94da87751b4706cddcbeee6239bde9d1b2a01
Eunjeong Park <ejisvip@gmail.com>
2019-04-29 01:37:01 +0900

    Wrap a REST API with GraphQL & node-fetch

--- movieql/graphql/resolvers.js
+++ movieql/graphql/resolvers.js
@@ -1,7 +1,15 @@
+// import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./movie_db";
+
 const resolvers = {
   Query: {
-    name: () => "nicolas"
+    movies: (_, { rating, limit }) => getMovies(limit, rating)
+    // movie: (_, { id }) => getById(id)
   }
+  // Mutation: {
+  //   addMovie: (_, { name, score }) => addMovie(name, score),
+  //   deleteMovie: (_, { id }) => deleteMovie(id),
+  // }
 };
 
 export default resolvers;
