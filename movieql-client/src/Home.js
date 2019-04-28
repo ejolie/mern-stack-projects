8c9d1efe2a66bbdab16a6e72f611b9e33865c263
Eunjeong Park <ejisvip@gmail.com>
2019-04-29 03:03:12 +0900

    Get data from GraphQL API

--- movieql-client/src/Home.js
+++ movieql-client/src/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => "Home";
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, data, error }) => {
+      if (loading) return "Loading...";
+      if (error) return "Something happened.";
+      return data.movies.map(movie => (
+        <h2 key={movie.id}>
+          {movie.title} / {movie.rating}
+        </h2>
+      ));
+    }}
+  </Query>
+);
 
 export default Home;
