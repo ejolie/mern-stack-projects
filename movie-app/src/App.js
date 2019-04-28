f2a62cad7e52694b56c7952f506198250683baf6
Eunjeong Park <ejisvip@gmail.com>
2019-04-29 02:17:48 +0900

    Fix a bug

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -3,39 +3,44 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps -> shouldComponentUpdate() 
+  // -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {}
+  state = {};
 
   componentDidMount() {
     this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie 
-        title={movie.title_english} 
-        poster={movie.medium_cover_image} 
-        key={movie.id} 
-        genres={movie.genres}
-        synopsis={movie.synopsis}
-      />
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie 
+          title={movie.title_english} 
+          poster={movie.large_cover_image} 
+          key={movie.id} 
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
     });
     return movies;
   }
 
-_getMovies = async () => {
+  _getMovies = async () => {
     const movies = await this._callApi();
     this.setState({
       movies
     })
-  }
+  };
 
   _callApi = () => {
-    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count?sort_by=download_count")
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
       .then(res => res.json())
       .then(json => json.data.movies)
-      .catch(err => console.log(err))
-  }
+      .catch(err => console.log(err));
+  };
 
   render() {
     console.log('App - rendered');
--- movie-app/src/Movie.js
+++ movie-app/src/Movie.js
@@ -6,15 +6,15 @@ import './Movie.css';
 const Movie = ({title, poster, genres, synopsis}) => {
   return ( 
     <div className="Movie">
-      <div className="Movie_Columns">
+      <div className="Movie__Columns">
         <MoviePoster poster={poster} alt={title} />
       </div>
-      <div className="Movie_Columns">
+      <div className="Movie__Columns">
         <h1>{title}</h1>
-        <div className="Movie_Genres">
-        {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+        <div className="Movie__Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
         </div>
-        <div className="Movie_Synopsis">
+        <div className="Movie__Synopsis">
         <LineEllipsis 
           text={synopsis}
           maxLine='3'
@@ -27,18 +27,24 @@ const Movie = ({title, poster, genres, synopsis}) => {
     </div>
   );
 }
- 
-Movie.proptypes = {
-  title: PropTypes.string.isrequired,
-  poster: PropTypes.string.isRequired,
-  genres: PropTypes.array.isRequired,
-  synopsis: PropTypes.string.isRequired
-}
 
 const MoviePoster = ({poster, alt}) => {
   return ( 
-    <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
   );
+}
+
+const MovieGenre = ({genre}) => {
+  return (
+    <span className="Movie__Genre">{genre}</span>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
@@ -46,14 +52,8 @@ MoviePoster.propTypes = {
   alt: PropTypes.string.isRequired
 }
 
-const MovieGenre = ({genre}) => {
-  return (
-    <span className="Movie_Genre">{genre}</span>
-  );
-}
-
 MovieGenre.propTypes = {
   genre: PropTypes.string.isRequired
 }
- 
+
 export default Movie;
