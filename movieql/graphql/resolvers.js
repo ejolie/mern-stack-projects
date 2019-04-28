24d5105c51eb4efa534658503af263fbc2634444
Eunjeong Park <ejisvip@gmail.com>
2019-04-29 01:58:10 +0900

    Refactor node-fetch to axios

--- movieql/graphql/resolvers.js
+++ movieql/graphql/resolvers.js
@@ -1,9 +1,12 @@
 // import { getMovies, getById, addMovie, deleteMovie } from "./db";
-import { getMovies } from "./movie_db";
+// import { getMovies } from "./movie_db";
+import { getMovies, getMovie, getSuggestions } from "./movie_db_axios";
 
 const resolvers = {
   Query: {
-    movies: (_, { rating, limit }) => getMovies(limit, rating)
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
     // movie: (_, { id }) => getById(id)
   }
   // Mutation: {
