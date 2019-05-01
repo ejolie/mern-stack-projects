84317c5c85140ff4d1327be7b0337156003880ba
Eunjeong Park <ejisvip@gmail.com>
2019-05-01 22:17:02 +0900

    Finish project and use Hooks

--- movieql-client/src/queries.js
+++ movieql-client/src/queries.js
@@ -7,6 +7,26 @@ export const HOME_PAGE = gql`
       title
       genres
       rating
+      medium_cover_image
+    }
+  }
+`;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetail($movieId: Int!) {
+    movie(id: $movieId) {
+      medium_cover_image
+      title
+      rating
+      description_intro
+      language
+    }
+
+    suggestions(id: $movieID) {
+      id
+      title
+      rating
+      medium_cover_image
     }
   }
 `;
