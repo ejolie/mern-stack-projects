c7e618980f041c9c6292559c25c73330ec8c2c84
Eunjeong Park <ejisvip@gmail.com>
2019-09-14 14:16:23 +0900

    Finish 4장. API 연동하기

---
+++ api-integrate/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Users from './Users-useAsync';
+import { UsersProvider } from './UsersContext';
+
+function App() {
+  return (
+    <UsersProvider>
+      <Users />
+    </UsersProvider>
+  );
+}
+
+export default App;
